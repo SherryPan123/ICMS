4b1bafa643ff851010d68004adf78f86a098e54f
Sherry <281850417@qq.com>
2016-03-19 17:44:40 +0800

    make common files and folders

---
+++ src/main/java/com/database/icms/controller/ConditionsController.java
@@ -0,0 +1,10 @@
+package com.database.icms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/conditions")
+public class ConditionsController {
+
+}
--- src/main/webapp/WEB-INF/views/user/list.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" trimDirectiveWhitespaces="true"%>
-<html>
-<body>
-<h2>hello</h2>
-</body>
-</html>
